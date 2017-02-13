fae8c7c8373efd59936ff46508140db3cbd30e48
Owan1 <danirocar@hotmail.com>
2017-02-13 17:23:44 +0100

    Vistas FrontOffice

---
+++ resources/views/octagon_layout/octagon_left_side/octagon_rated.blade.php
@@ -0,0 +1,97 @@
+<!-- ___Start Top Rated Area___ -->
+<div class="top-rated-area toogle-sidebar-sections">
+    <div class="top-rated-head">
+        <a href="#0" class="accordion-toggle">Top Rated <span class="toggle-icon"><i class="fa fa-bars"></i></span></a>
+        <div class="accordion-content">
+
+            <!-- ___Start Top Rated Body___ -->
+            <div class="top-rated-body">
+
+                <a href="#0" class="top-rated-item javascript">
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%;">
+                        </div>
+                    </div>
+                    <div class="top-rated-heading">JavaScript</div>
+
+                    <div class="top-rated-count">
+                        <div class="progress-score">
+                            <i class="fa fa-eye"></i>
+                            <span>2564</span>
+                        </div>
+                    </div>
+                </a>
+                <!-- End Java Script -->
+
+                <a href="#0" class="top-rated-item iphone">
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="23" aria-valuemin="0" aria-valuemax="100" style="width: 23%;">
+                        <span class="sr-only">60% Complete</span>
+                        </div>
+                    </div>
+                    <div class="top-rated-heading">Iphone</div>
+
+                    <div class="top-rated-count">
+                        <div class="progress-score">
+                            <i class="fa fa-eye"></i>
+                            <span>2564</span>
+                        </div>
+                    </div>
+                </a>
+                <!-- End Iphone -->
+
+                <a href="#0" class="top-rated-item top-hills">
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="43" aria-valuemin="0" aria-valuemax="100" style="width: 43%;">
+                        <span class="sr-only">60% Complete</span>
+                        </div>
+                    </div>
+                    <div class="top-rated-heading">Top 10 Hills</div>
+
+                    <div class="top-rated-count">
+                        <div class="progress-score">
+                            <i class="fa fa-eye"></i>
+                            <span>2564</span>
+                        </div>
+                    </div>
+                </a>
+                <!-- End Top Hills -->
+
+                <a href="#0" class="top-rated-item wordpress">
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="21" aria-valuemin="0" aria-valuemax="100" style="width: 21%;">
+                        <span class="sr-only">60% Complete</span>
+                        </div>
+                    </div>
+                    <div class="top-rated-heading">Wordpress</div>
+
+                    <div class="top-rated-count">
+                        <div class="progress-score">
+                            <i class="fa fa-eye"></i>
+                            <span>2564</span>
+                        </div>
+                    </div>
+                </a>
+                <!-- End Wordpress -->
+
+                <a href="#0" class="top-rated-item ebola-virus">
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="22" aria-valuemin="0" aria-valuemax="100" style="width: 22%;">
+                        <span class="sr-only">60% Complete</span>
+                        </div>
+                    </div>
+                    <div class="top-rated-heading">Ebola Virus</div>
+
+                    <div class="top-rated-count">
+                        <div class="progress-score">
+                            <i class="fa fa-eye"></i>
+                            <span>2564</span>
+                        </div>
+                    </div>
+                </a><!-- End Ebola Virus -->
+
+            </div><!-- End Top Rated Body -->
+        </div><!-- End Accordion Content -->
+    </div><!-- End Top Rated Head -->
+</div>
+<!-- End Top Rated Area -->
---
+++ resources/views/octagon_layout/octagon_top_bar/octagon_top_submenu.blade.php
@@ -0,0 +1,34 @@
+<!-- ___Start Menu Wrapper___ -->
+<div class="menu-wrapper">
+    <div class="post-tab container-fluid no-padding">
+        <div role="tabpanel">
+            <div class="row margin-zero">
+                <div class="col-md-3 col-sm-3 no-padding">
+                    <div class="submenu-lists">
+                        <h4>Style News <i class="fa fa-leaf"></i></h4>
+                        <!-- ___Nav Tabs___ -->
+                        <div class="nav nav-tabs" role="tablist">
+                            <div class="list active" role="presentation"><a href="#beauty-care" aria-controls="beauty-care" role="tab" data-toggle="tab">Beauty Care</a></div>
+
+                            <div class="list" role="presentation"><a href="#hair-style" aria-controls="hair-style" role="tab" data-toggle="tab">Hair Style</a></div>
+
+                            <div class="list" role="presentation"><a href="#spa" aria-controls="spa" role="tab" data-toggle="tab">Spa</a></div>
+
+                            <div class="list" role="presentation"><a href="#cosmetics" aria-controls="cosmetics" role="tab" data-toggle="tab">Cosmetics</a></div>
+
+                            <div class="list" role="presentation"><a href="#facial" aria-controls="facial" role="tab" data-toggle="tab">Facial</a></div>
+
+                            <div class="list" role="presentation"><a href="#skin" aria-controls="skin" role="tab" data-toggle="tab">Skin</a></div>
+                        </div><!-- End Nav Tab -->
+                    </div><!-- End Sub Menu List -->
+                </div><!-- End Column -->
+
+                <div class="col-md-9 col-sm-9">
+                    <!-- ___Tab Content___ -->
+
+                    <!-- End Tab Content-->
+                </div><!-- End Column -->
+            </div><!-- End Row -->
+        </div><!-- End Tab Panel -->
+    </div><!-- End Post Tab -->
+</div><!-- End Menu Wrapper -->
