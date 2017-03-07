396971f8b58c63466b025ff40e43c6b1a212b683
GitCarlosMoreno <carlos.mogui95@gmail.com>
2017-03-07 19:07:47 +0100

    Imagenes Back office

--- public/js/ImgMuestra.js
+++ public/js/ImgMuestra.js
@@ -1,14 +1,14 @@
 $(document).onload( function(){
-//	document
+	$("#img").click(function() {
 		//if(input.files && input.files[0]){
 		//	var reader = new FileReader();
 
 			//reader.onload = function (e){
-			    $("imgmuestra").atrr('src', "pescado");
+			    $("#imgmuestra").atrr('src', "pescado");
 			//}
 
 		//reader.readAsDataURL(input.files[0]);
 		//}
 		
-	
+	})
 });
