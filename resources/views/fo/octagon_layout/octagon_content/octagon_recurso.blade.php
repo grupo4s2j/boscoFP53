e0d311a5bc72d756963089d8254d3884f80a86b6
Owan1 <danirocar@hotmail.com>
2017-03-08 19:10:41 +0100

    URL img

--- resources/views/fo/octagon_layout/octagon_content/octagon_recurso.blade.php
+++ resources/views/fo/octagon_layout/octagon_content/octagon_recurso.blade.php
@@ -1,6 +1,6 @@
 <div class="col-md-6">
     <div class="post-bottom-content">
-        <a href="{{ url('recursos/' . $recurso->id) }}"><img class="img-responsive" src="{{ asset('img/recursos/' . $recurso->img) }}" alt=""></a>
+        <a href="{{ url('recursos/' . $recurso->id) }}"><img class="img-responsive" src="{{ asset('./img/recursos/' . $recurso->img) }}" alt=""></a>
         <a href="{{ url('recursos/'. $recurso->id) }}"><h3>{{$recurso->titulo}}</h3></a>
         <h3><span class="travel-bg">4<a class="travel-bg" href="{{ url('recursos/'. $recurso->id) }}">4 comment</a></span></h3>
         <span class="date">{{$recurso->fechaPosteo}}</span>
