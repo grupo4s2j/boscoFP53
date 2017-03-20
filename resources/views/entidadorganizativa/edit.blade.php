40dfb227f798960fbe217f54d69ab136241c6f27
Gitkikaso <grupo4s2j@gmial.com>
2017-03-20 17:12:34 +0100

    puto google masp y su puta madre

--- resources/views/entidadorganizativa/edit.blade.php
+++ resources/views/entidadorganizativa/edit.blade.php
@@ -19,21 +19,38 @@
             nombre!!}" required>
         </div>
         <div class="form-group">
-            <label for="direccion">direccion</label>
-            <input id="direccion" name = "direccion" type="text" class="form-control" value="{!!$entidadorganizativa->
-            direccion!!}"> 
+            <label for="geolocalizacion">Direccion</label>
+            <input id="address" name="direccion" type="text" class="form-control" placeholder="Direccion"  value="{!!$entidadorganizativa->
+            direccion!!}"required>
+            <input id="geolocalizacion" name="geolocalizacion" type="text" class="form-control" placeholder="Geoposicion"  value="{!!$entidadorganizativa->
+            geolocalizacion!!}"required>
+
         </div>
+        {{-- <div class="form-group">
+             <label for="activo">activo</label>
+             <input id="activo" name = "activo" type="text" class="form-control">
+         </div>--}}
+        <button class='btn btn-success ' type='submit'>Create</button>
         <div class="form-group">
-            <label for="geolocalizacion">geolocalizacion</label>
-            <input id="geolocalizacion" name = "geolocalizacion" type="text" class="form-control" value="{!!$entidadorganizativa->
-            geolocalizacion!!}" required>
+            <label for="direccion">Buscar direccion</label>
+            <div class="row-lg-12">
+                <div class="col-lg-10">
+                    <input id="input-address" name="direccion" type="text" class="form-control" >
+                </div>
+                <div class="col-lg-2">
+                    <button id="search-address" type="button" class="btn btn-primary ">Buscar</button>
+
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <div id="gmap" class="mapHelper__div" style="width: 100%; height: 400px"></div>
+                </div>
+            </div>
         </div>
-        {{--<div class="form-group">
-            <label for="activo">activo</label>
-            <input id="activo" name = "activo" type="text" class="form-control" value="{!!$entidadorganizativa->
-            activo!!}">
-        </div>--}}
-        <button class = 'btn btn-primary' type ='submit'>Update</button>
     </form>
+    <script src="http://maps.google.com/maps/api/js?key=AIzaSyAv0cR_5TFppsd9IH6GBPhmXl6jL-R2KwQ"></script>
+    <script src="/js/updateLocations.js"></script>
 </section>
 @endsection
