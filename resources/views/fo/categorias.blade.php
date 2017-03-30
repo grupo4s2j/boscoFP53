ac5c1e022d70f383b8d8d1d7f94dae146c1f7deb
Owan1 <danirocar@hotmail.com>
2017-03-30 15:56:09 +0200

    Update Menu Top Categorias

--- resources/views/fo/categorias.blade.php
+++ resources/views/fo/categorias.blade.php
@@ -25,14 +25,15 @@
 
         </div> <!-- row-->			    					                                     	 
     </section>
-    @section('scripts')
-    <!--<script>
-        $("a h2").hover(function(){
-            $(this).animate({fontSize: "26px"}, 400)
-        }, function() {
-            $(this).animate({fontSize: "24px"}, 400)  
-        })
-    </script>-->
-    @endsection
+@endsection
 
+
+@section('scripts')
+<!--<script>
+    $("a h2").hover(function(){
+        $(this).animate({fontSize: "26px"}, 400)
+    }, function() {
+        $(this).animate({fontSize: "24px"}, 400)  
+    })
+</script>-->
 @endsection
